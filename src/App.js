0598ae121e24d3c7a91d4f7675bbdeaaa4c7738a
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-16 10:26:11 +0200

    jsx-01-

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const hello = "Hello, World!";
+const hello = <h1>Hello, World!;</h1>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{hello}</h1>
+        {hello}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
