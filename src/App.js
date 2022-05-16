f1057dbae7bfc37500ea5e26190581679bf80e4b
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-16 10:05:12 +0200

    jsx-02-

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./Welcome";
 
-const hello = "Hello, World!";
+const hello = <h1>Hello, World!</h1>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{hello}</h1>
+        {hello}
         <Welcome />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
