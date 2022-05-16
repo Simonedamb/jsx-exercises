84e51dde4ec7ac2720e6a660ec3975b195b09c0f
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-16 10:24:30 +0200

    jsx-02-

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Welcome from "./Welcome";
 
 const hello = <h1>Hello, World!</h1>;
+
+const welcome = (name) => {
+  return <h1>{name}</h1>;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {hello}
-        <Welcome />
+        {welcome("simone")}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Hello = (name) => {
-  name = "Simone";
-  return <h1>{name}</h1>;
-};
-
-export default Hello;
